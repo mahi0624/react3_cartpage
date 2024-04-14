33377012ff2ac908762ba47d2a8a22435a6eb97e
mahi0624 <71305220+mahi0624@users.noreply.github.com>
2024-04-14 17:18:45 +0530

    Update Cartcontainer.jsx

--- CartPageNew/src/Components/Cart/Cartcontainer.jsx
+++ CartPageNew/src/Components/Cart/Cartcontainer.jsx
@@ -20,25 +20,25 @@ const Cartcontainer = () => {
 }
 
 export default Cartcontainer
-// import React from 'react'
-// import Cartitem from './Cartitems'
-// import { useState } from 'react';
+import React from 'react'
+import Cartitem from './Cartitems'
+import { useState } from 'react';
 
-// let cartItem = [
-//     { id: 1, label: "Noodles", price: 30, quantity: 1 },
-//     { id: 2, label: "Biryani", price: 90, quantity: 1 },
-//     { id: 3, label: "Chips", price: 10, quantity: 1 },
-//   ];
+let cartItem = [
+    { id: 1, label: "Noodles", price: 30, quantity: 1 },
+    { id: 2, label: "Biryani", price: 90, quantity: 1 },
+    { id: 3, label: "Chips", price: 10, quantity: 1 },
+  ];
 
-// const Cartcoontainer = () => {
-//     const[cart,setCart] =useState(cartItem);
-//   return (
-//      <>
-//      {cart.map((item)=>(
-//         <Cartitem key={item.id} {...item}/>
-//     ))}
-//      </>
-//   )
-// }
+const Cartcoontainer = () => {
+    const[cart,setCart] =useState(cartItem);
+  return (
+     <>
+     {cart.map((item)=>(
+        <Cartitem key={item.id} {...item}/>
+    ))}
+     </>
+  )
+}
 
-// export default Cartcoontainer
+export default Cartcoontainer
